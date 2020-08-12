9ae3f006ac300e500e2e452aedaaeb3cb824e76b
Kutlu Ata <32374530+leto666@users.noreply.github.com>
2020-08-12 14:08:39 +0300

    Add files via upload

---
+++ precache-manifest.7015ad0f406973c6029567f893c77451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c57a9bbcbe2c0467155731b9dbb377",
+    "url": "/resimlervecekenler/index.html"
+  },
+  {
+    "revision": "5596f5d774b504734d79",
+    "url": "/resimlervecekenler/static/css/main.a96784ca.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/resimlervecekenler/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resimlervecekenler/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5596f5d774b504734d79",
+    "url": "/resimlervecekenler/static/js/main.76c9c5e2.chunk.js"
+  },
+  {
+    "revision": "a19636bb94d40e925517",
+    "url": "/resimlervecekenler/static/js/runtime-main.170b0014.js"
+  }
+]);
